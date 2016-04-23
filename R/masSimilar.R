4a019e932aabe3c75ec6b17864908f08b91fb735
Víctor Tinoco <victor.tinoco.86@gmail.com>
2016-04-23 16:14:33 +0200

    Create masSimilar.R

---
+++ R/masSimilar.R
@@ -0,0 +1,23 @@
+# Se implementa una una función masSimilar cuyo objetivo será encontrar el documento de un corpus de tm 
+# que sea más similar a una determinada consulta haciendo uso de la función dissimilarity
+dissimilarity <- function(x, y, method="cosine") {  
+         corp <- Corpus(VectorSource(c(x)))
+         corp <- c(corp, y)
+         corp <- tm_map(corp, content_transformer(tolower))
+         corp <- tm_map(corp, removeWords, stopwords("english"))
+         corp <- tm_map(corp, removePunctuation)
+         corp <- tm_map(corp, removeNumbers)
+         corp <- tm_map(corp, stemDocument)
+         corp <- tm_map(corp, PlainTextDocument)
+         tdm <- TermDocumentMatrix(corp)
+         proxy::dist(as.matrix(t(tdm)), method = method)
+   	}
+
+
+masSimilar <- function(x, y){
+		h <- dissimilarity(x,y)   # buscamos los grados de disimilaridad
+		t <- length(y) 		  	# Lo buscamos en la primera columna que nos devuelve h
+		best = which.min(h[1:t])	# Utilizamos una de las funciones definidas en R, para mejorar rendimiento.
+		print (best)
+		print (as.character(y[[best]]))
+	}
